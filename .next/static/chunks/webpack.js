48981d1c482c597f0dede8117c15e4b82e33043e
Moin105 <moinlatif105@gmail.com>
2022-06-24 17:57:49 +0500

    responsive fixes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e584f4de39867475"; }
+/******/ 		__webpack_require__.h = function() { return "145d702afc380219"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

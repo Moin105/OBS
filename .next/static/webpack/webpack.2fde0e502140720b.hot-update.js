03c34b8880d0c166cbcbcacd6e9d844150c8c119
Moin105 <moinlatif105@gmail.com>
2022-06-24 17:28:27 +0500

    updated UI/UX

--- .next/static/webpack/webpack.2fde0e502140720b.hot-update.js
+++ .next/static/webpack/webpack.2fde0e502140720b.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "985fd5a2a0662274"; }
+/******/ 	__webpack_require__.h = function() { return "a1a507e3ba2fa609"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */

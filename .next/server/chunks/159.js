7ee4c8a6656ddeca3797b1a9efa927e1db9c46f5
Moin105 <moinlatif105@gmail.com>
2022-06-24 18:48:52 +0500

    updated UI/UX

---
+++ .next/server/chunks/159.js
@@ -0,0 +1,309 @@
+exports.id = 159;
+exports.ids = [159];
+exports.modules = {
+
+/***/ 6208:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"container": "Home_container__97eC3",
+	"header": "Home_header__CyQ_T",
+	"hlogo": "Home_hlogo__g6HPf",
+	"btn": "Home_btn__gqI1M",
+	"wrapper": "Home_wrapper__XaPDZ",
+	"banner_container": "Home_banner_container__dXTOj",
+	"banner": "Home_banner__F5GOV",
+	"bl": "Home_bl__kyDNC",
+	"bannerp": "Home_bannerp__yOiLw",
+	"b": "Home_b__0qEoG",
+	"black": "Home_black__GozVU",
+	"cover": "Home_cover__KDvzz",
+	"br": "Home_br__qEmGa",
+	"content": "Home_content__WcTpR",
+	"yh": "Home_yh__XIQxM",
+	"bh": "Home_bh__Lz7AM",
+	"layout": "Home_layout__CCYK0",
+	"left": "Home_left__PDvAo",
+	"center": "Home_center__qt9SV",
+	"mobile": "Home_mobile__NYQwK",
+	"right": "Home_right__Q3ZHY",
+	"box": "Home_box__1j7_f",
+	"figure": "Home_figure__UYuHu",
+	"detail": "Home_detail__3he7u",
+	"wrw": "Home_wrw__lr3Pm",
+	"wrwp": "Home_wrwp__xxPpz",
+	"wrwbg": "Home_wrwbg__1mp_S",
+	"portfolio": "Home_portfolio__ce2IP",
+	"pleft": "Home_pleft__CDMRv",
+	"pright": "Home_pright__S_uyh",
+	"background": "Home_background__EMQXR",
+	"testimonials": "Home_testimonials__cD4Ew",
+	"quote": "Home_quote__bV7Ma",
+	"iq": "Home_iq__BCnmH",
+	"review": "Home_review__Bgd_m",
+	"line": "Home_line__tAatH",
+	"cname": "Home_cname__FvT0J",
+	"star": "Home_star__JQ7yF",
+	"group": "Home_group__P_ABr",
+	"dot": "Home_dot__d_3rI",
+	"carousel": "Home_carousel__mZQjq",
+	"inner": "Home_inner__k19bH",
+	"carousel_item": "Home_carousel_item__0x_Vj",
+	"carousel-status": "Home_carousel-status__oxBN1",
+	"ourteam": "Home_ourteam__xHYS9",
+	"yhc": "Home_yhc__sJW8t",
+	"bhc": "Home_bhc__uX_cx",
+	"row_container": "Home_row_container__of9MF",
+	"card": "Home_card__K7aTN",
+	"picture": "Home_picture__bY6eE",
+	"yc": "Home_yc__xXKOF",
+	"cn": "Home_cn__35dZt",
+	"cl": "Home_cl__wRoSI",
+	"position": "Home_position__F3kBL",
+	"contact": "Home_contact__Tvs4T",
+	"waves": "Home_waves__ECT6y",
+	"contact_us": "Home_contact_us__OFc6S",
+	"contl": "Home_contl__zUoMj",
+	"contact_cell": "Home_contact_cell__uWBuN",
+	"form": "Home_form__eFLbn",
+	"row": "Home_row__s8Nhb",
+	"message": "Home_message__SarrE",
+	"formtbtn": "Home_formtbtn__GKIPg",
+	"footer": "Home_footer__zed0_",
+	"b1": "Home_b1__I28mi",
+	"logos": "Home_logos__Ds4yv",
+	"b2": "Home_b2__GLTxw",
+	"hadd": "Home_hadd__QqIAL",
+	"address": "Home_address__axVNW",
+	"email": "Home_email__Lonpu",
+	"footer_row": "Home_footer_row__gvQ12",
+	"bottom_footer": "Home_bottom_footer__TYAQS",
+	"portfolio_wrapper": "Home_portfolio_wrapper__OFul_",
+	"ph": "Home_ph__0R6y0",
+	"text": "Home_text__8moDw",
+	"c1": "Home_c1__FxBk_",
+	"conr": "Home_conr__Ofg_P",
+	"vector": "Home_vector__gW6qQ",
+	"conl": "Home_conl__zFHN2",
+	"c2": "Home_c2__mEJRK",
+	"pwork": "Home_pwork__gFa_k",
+	"span": "Home_span__q9_wN"
+};
+
+
+/***/ }),
+
+/***/ 5508:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/logo.bb7fce3a.png","height":105,"width":186,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAFCAYAAAB4ka1VAAAAoUlEQVR42hWNwQ7BQBRFX9spP+VbrHyFLVsiqYRUbISdTCL6ByXSdpQOfsHWQmM61XZci5v3Xu6591F+ak3qzHqUqR1qwcLmRkGRsN33Ym9rSSvSZ+dljCGAPgwJY1MIFlVXa91I6hOOp7lTR8Xu/g+UqTOFAgR7VWbl9D62u1gOSI4Be4AXAIdo4HgxIJWwEYzlJ3JnaOGYvoqZB3EtnPkPKGph8X6xyREAAAAASUVORK5CYII="});
+
+/***/ }),
+
+/***/ 6675:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5675);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_image__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(6208);
+/* harmony import */ var _styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var _public_logo_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(5508);
+
+
+
+
+
+function Footer() {
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+            className: (_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_4___default().wrapper),
+            id: "about",
+            children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: (_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_4___default().footer),
+                children: [
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: (_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_4___default().footer_row),
+                        children: [
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                className: (_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_4___default().b1),
+                                children: [
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("figure", {
+                                        className: (_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_4___default().logos),
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_image__WEBPACK_IMPORTED_MODULE_2___default()), {
+                                            src: _public_logo_png__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z,
+                                            alt: "OBS TECHNOLOGIA",
+                                            layout: "fill",
+                                            objectFit: "contain"
+                                        })
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                        children: "OBS Technologia is one of the fastest-growing IT companies providing various solutions and services across industries"
+                                    })
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                className: (_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_4___default().b2),
+                                children: [
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h3", {
+                                        children: "Call us"
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                        children: "051-8892643"
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                        children: "0304-4366760"
+                                    })
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                className: (_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_4___default().b2),
+                                children: [
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h3", {
+                                        className: (_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_4___default().hadd),
+                                        children: "Address"
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                        className: (_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_4___default().address),
+                                        children: "Office # 2, 3rd Floor Huzaifah Centre I-8 Markaz, Islamabad"
+                                    })
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                className: (_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_4___default().b2),
+                                children: [
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h3", {
+                                        children: "Email"
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                        className: (_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_4___default().email),
+                                        children: "Ceo@obstechnologia.com"
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                        className: (_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_4___default().email),
+                                        children: "hr@obstechnologia.com"
+                                    })
+                                ]
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                        className: (_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_4___default().bottom_footer),
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                            children: "@2021 FUTURE,OBS Technologia,All Rights Reserved"
+                        })
+                    })
+                ]
+            })
+        })
+    });
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Footer);
+
+
+/***/ }),
+
+/***/ 8492:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _public_logo_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5508);
+/* harmony import */ var _styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(6208);
+/* harmony import */ var _styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(5675);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_image__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(1664);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_4__);
+
+
+
+
+
+
+function Header() {
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("header", {
+            className: (_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_5___default().header),
+            children: [
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("figure", {
+                    className: (_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_5___default().hlogo),
+                    children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_image__WEBPACK_IMPORTED_MODULE_3___default()), {
+                        src: _public_logo_png__WEBPACK_IMPORTED_MODULE_2__/* ["default"] */ .Z,
+                        alt: "OBS TECHNOLOGIA",
+                        layout: "fill",
+                        objectFit: "contain"
+                    })
+                }),
+                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("ul", {
+                    className: (_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_5___default().center_nav),
+                    children: [
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                            children: "Agency"
+                        }),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                            children: "Portfolio"
+                        }),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_4___default()), {
+                                href: "#services",
+                                children: "Services "
+                            })
+                        }),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_4___default()), {
+                                href: "#contact",
+                                smooth: "true",
+                                children: "Contact"
+                            })
+                        })
+                    ]
+                }),
+                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("ul", {
+                    className: "right-nav",
+                    children: [
+                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                            children: [
+                                " ",
+                                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_4___default()), {
+                                    href: "#about",
+                                    smooth: "true",
+                                    children: "About"
+                                })
+                            ]
+                        }),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                            className: (_styles_Home_module_scss__WEBPACK_IMPORTED_MODULE_5___default().btn),
+                            children: "Let's Talk"
+                        })
+                    ]
+                })
+            ]
+        })
+    });
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Header);
+
+
+/***/ })
+
+};
+;
